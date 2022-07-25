0c1e350fd69fdafa1de50285301837d25a509a64
Julian Gründner <juliangruendner@googlemail.com>
2022-07-25 18:44:52 +0200

    Initialise with codex test queries

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # fhir-codex-test
+
+Adjust lines 10-13 in `fhir_codex_check.py` to your requirements and
+execute with `python3 fhir_codex_check.py`
+
+This will generate a `codex-check-results.csv` file in your repository
+
+## Requirements
+
+Python3 and the packages: json, requests, urllib, csv
---
+++ fhir_codex_check.py
@@ -0,0 +1,72 @@
+
+import json
+import requests
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+from json.decoder import JSONDecodeError
+from requests.auth import HTTPBasicAuth
+import csv
+
+fhir_user = None
+fhir_pw = None
+fhir_base_url = 'http://localhost:8081/fhir'
+
+
+def query_successful(query_url, resp_links):
+
+    self_link = ""
+    for link in resp_links:
+        if link['relation'] == 'self':
+            self_link = link['url']
+
+    parsed_url = urlparse(query_url)
+    query_url_params = parse_qs(parsed_url.query)
+
+    parsed_url = urlparse(self_link)
+    self_link_params = parse_qs(parsed_url.query)
+
+    for param in query_url_params:
+        if param not in self_link_params:
+            return False
+
+    return True
+
+def execute_query(query):
+
+    query_url = f'{fhir_base_url}{query}'
+    resp = requests.get(query_url, headers={"Prefer": 'handling=strict'}, auth=HTTPBasicAuth(
+        fhir_user, fhir_pw))
+
+    resp_object = {}
+    resp_object['status'] = "success"
+
+    if resp.status_code != 200:
+        resp_object['status'] = "failed"
+
+    try:
+        resp_object['json'] = resp.json()
+
+        if 'link' in resp_object['json'].keys() and not query_successful(query_url, resp_object['json']['link']):
+            resp_object['status'] = "failed"
+    except JSONDecodeError:
+        resp_object['status'] = "failed"
+
+    return resp_object
+
+
+with open("codex-test-queries.json", 'r') as f:
+    json_input = json.load(f)
+    with open('codex-check-results.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=';',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        header = ['Name', 'Code', 'System', 'Status', 'Number Results', 'FHIR Search']
+        csv_writer.writerow(header)
+        for query in json_input:
+            query_result = execute_query(query['query'])
+
+            if query_result['status'] != "failed":
+                row_to_write = [query['name'],query['code'], query['system'],'sucess', query_result['json']['total'],query['query_decoded']]
+            else:
+                row_to_write = [query['name'],query['code'], query['system'],'failed','', query['query_decoded']]
+
+            csv_writer.writerow(row_to_write)
